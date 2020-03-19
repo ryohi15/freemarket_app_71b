a78c1abc84ed1e910e39e417291e0df1cbfb3d08
curtaincall888 <kota3730@gmail.com>
2020-03-19 21:38:47 +0900

    add varitation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :password, confirmation: { case_sensitive: false }
+  validates :password, confirmation: { case_sensitive: false }, length: { minimum: 7 }
   validates :password_confirmation, presence: true
-  validates :name, presence: true, length: { minimum: 7 }
+  validates :nickname, presence: true
+  validates :last_name, presence: true,
+    format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力して下さい" }
+  validates :first_name, presence: true,
+    format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "全角で入力して下さい" }
+  validates :last_name_kana, presence: true,
+    format: { with: /\A[ぁ-んー－]+\z/, message: "全角ひらがなで入力して下さい" }
+  validates :first_name_kana, presence: true,
+    format: { with: /\A[ぁ-んー－]+\z/, message: "全角ひらがなで入力して下さい" }
+  validates :birth_year, presence: true
+  validates :birth_month, presence: true
+  validates :birth_day, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_103305) do
+ActiveRecord::Schema.define(version: 2020_03_19_123545) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "last_name", null: false
