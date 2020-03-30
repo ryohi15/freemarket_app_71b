4d0c01359aec4779e8e1a0794c0b8cb701cf010c
kazuki-oda <oda.kazuki19970317@gmail.com>
2020-03-30 20:36:43 +0900

    3

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,16 @@
 class Item < ApplicationRecord
+  validates :user_id, presence: true
+  validates :category_id, presence: true
+  validates :seller_id,  presence: true
+  validates :name, presence: true
+  validates :content, presence: true
+  validates :status, presence: true
+  validates :price, presence: true
+  validates :cost, presence: true
+  validates :date, presence: true
+  accepts_nested_attributes_for :images
 
+  belongs_to :user
+  belongs_to :category
+  has_many :images
 end
--- db/migrate/20200330095958_create_items.rb
+++ db/migrate/20200330095958_create_items.rb
@@ -1,17 +1,17 @@
 class CreateItems < ActiveRecord::Migration[5.2]
   def change
     create_table :items do |t|
-      t.integer :user_id
-      t.integer :category_id
-      t.integer :buyer_id
-      t.string :name
+      t.integer :user_id, null: false, foreign_key: true
+      t.integer :category_id, null: false, foreign_key: true
+      t.integer :buyer_id,  foreign_key: true
+      f.integer :seller_id, null: false, foreign_key: true
+      t.string :name, null: false
       t.string :brand
-      t.text :content
-      t.string :status
-      t.integer :price
-      t.string :cost
-      t.string :date
-
+      t.text :content, null: false
+      t.string :status, null: false
+      t.integer :price, null: false
+      t.string :cost, null: false
+      t.string :date, null: false
       t.timestamps
     end
   end
