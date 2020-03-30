7d300d26d105d394774cc6dd32bdfc6d99332c93
curtaincall888 <kota3730@gmail.com>
2020-03-30 19:19:55 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -23,26 +23,6 @@ ActiveRecord::Schema.define(version: 2020_03_27_100957) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "images", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "item_id", null: false
-    t.string "image", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
-    t.integer "category_id", null: false
-    t.string "name", null: false
-    t.text "content", null: false
-    t.string "status", null: false
-    t.integer "price", null: false
-    t.string "cost", null: false
-    t.string "date", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
