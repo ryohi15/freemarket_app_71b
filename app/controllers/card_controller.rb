4fbc54617e93221d66876344145106f8cfb365dc
koichiro1007 <koichiro1007@gmail.com>
2020-03-31 16:23:29 +0900

    修正しました
    @itemと@cardの変数をプライベートメソッドで定義し、before_actionでアクションを起こせるように変更しました。
    また、ハッシュロケット 部分はシンボル型へ変更しました。

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,4 +1,5 @@
 class CardController < ApplicationController
+  before_action :set_card, only: %i[delete show]
 
   require "payjp"
 
@@ -28,30 +29,29 @@ class CardController < ApplicationController
   end
 
   def delete #PayjpとCardデータベースを削除します
-    if set_card.blank?
-    else
+    unless @card.blank?
       Payjp.api_key = "sk_test_5da731fc8cfd2337a1058a3e"
       customer = Payjp::Customer.retrieve(set_card.customer_id)
       customer.delete
-      set_card.delete
+      @card.delete
     end
       redirect_to action: "new"
   end
 
   def show #Cardのデータpayjpに送り情報を取り出します
-    if set_card.blank?
+    if @card.blank?
       redirect_to action: "new" 
     else
       Payjp.api_key = "sk_test_5da731fc8cfd2337a1058a3e"
-      customer = Payjp::Customer.retrieve(set_card.customer_id)
-      @default_card_information = customer.cards.retrieve(set_card.card_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
   end
 
   private
 
   def set_card
-    Card.where(user_id: current_user.id).first
+    @card = Card.where(user_id: current_user.id).first
   end
 
 
--- app/controllers/purchase_controller.rb
+++ app/controllers/purchase_controller.rb
@@ -1,45 +1,44 @@
 class PurchaseController < ApplicationController
+  before_action :set_item, only: %i[pay]
+  before_action :set_card, only: %i[show pay]
 
   require 'payjp'
 
   def show
     @item = Item.find(params[:id])
-    card = Card.where(user_id: current_user.id).first
     #Cardテーブルは前回記事で作成、テーブルからpayjpの顧客IDを検索
-    if card.blank?
+    if @card.blank?
       #登録された情報がない場合にカード登録画面に移動
       redirect_to controller: "card", action: "new"
     else
       Payjp.api_key = "sk_test_5da731fc8cfd2337a1058a3e"
       #保管した顧客IDでpayjpから情報取得
-      customer = Payjp::Customer.retrieve(card.customer_id)
+      customer = Payjp::Customer.retrieve(@card.customer_id)
       #保管したカードIDでpayjpから情報取得、カード情報表示のためインスタンス変数に代入
-      @default_card_information = customer.cards.retrieve(card.card_id)
+      @default_card_information = customer.cards.retrieve(@card.card_id)
     end
   end
 
   def pay
     Payjp.api_key = "sk_test_5da731fc8cfd2337a1058a3e"
     Payjp::Charge.create(
-    :amount => set_item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
-    :customer => set_card.customer_id, #顧客ID
-    :currency => 'jpy', #日本円
+    amount: @item.price, #支払金額を入力（itemテーブル等に紐づけても良い）
+    customer: @card.customer_id, #顧客ID
+    currency: 'jpy', #日本円
   )
-  set_item.update( buyer_id: current_user.id)
+  @item.update( buyer_id: current_user.id)
   redirect_to action: 'done' #完了画面に移動
   end
 
   private
-
+  
   def set_item
-    Item.find(params[:id])
+    @item = Item.find(params[:id])
   end
 
   def set_card
-    Card.where(user_id: current_user.id).first
+    @card = Card.where(user_id: current_user.id).first
   end
-
-
 
 
 end
