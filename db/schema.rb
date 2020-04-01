bf1005b9151952f85048de2ffbeae7521f1bd0a1
itokeso <61177741+itokeso@users.noreply.github.com>
2020-04-01 18:50:22 +0900

    add-id

--- db/schema.rb
+++ db/schema.rb
@@ -54,13 +54,6 @@ ActiveRecord::Schema.define(version: 2020_04_01_080632) do
     t.string "name", null: false
     t.string "brand"
     t.text "content", null: false
-    t.string "status", null: false
-    t.integer "price", null: false
-    t.string "cost", null: false
-    t.string "date", null: false
-    t.string "name", null: false
-    t.string "brand"
-    t.text "content", null: false
     t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
