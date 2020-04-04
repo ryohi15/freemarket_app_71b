0ea47da7c2d2d15e2a222a3f4c436abcda22f34a
itokeso <61177741+itokeso@users.noreply.github.com>
2020-04-04 13:59:27 +0900

    add-image

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,11 +10,10 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
     @images = @item.images.build
-    @categoy = Category.new
     @category_parent_array = ["---"]
     Category.where(ancestry: nil).each do |parent|
     @category_parent_array << parent.name
-
+    
     def get_category_children
       @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
     end
@@ -22,16 +21,17 @@ class ItemsController < ApplicationController
     def get_category_grandchildren
       @category_grandchildren = Category.find("#{params[:child_id]}").children
     end
+    
 end
 
   end
 
   def create
     @item = Item.new(item_params)
-    if @item.save
+    if @item.save!
       redirect_to root_path notice: '出品しました'
     else
-      render :new
+      redirect_to new_item_path
     end
   end
 
@@ -44,5 +44,5 @@ end
     params.require(:item).permit(:category_id, :prefecture_id ,:brand, :name, :content, :status_id, :price, :cost_id, :delivery_day_id, :brand, :seller_id, :buyer_id, images_attributes: [:image])
   end
 
- 
+
 end
