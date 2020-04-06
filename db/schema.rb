f5496f994007ff77a82a9c068ef7b95d493663bd
itokeso <61177741+itokeso@users.noreply.github.com>
2020-04-06 17:31:24 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -49,13 +49,13 @@ ActiveRecord::Schema.define(version: 2020_04_03_061016) do
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "buyer_id"
+    t.integer "seller_id", null: false
     t.string "name", null: false
     t.string "brand"
     t.text "content", null: false
     t.integer "price", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "seller_id", null: false
     t.integer "prefecture_id"
     t.integer "status_id"
     t.integer "cost_id"
